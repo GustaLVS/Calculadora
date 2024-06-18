9b5237974a936d640e7b597d009ad773cf09a342
GustaLVS <168142217+GustaLVS@users.noreply.github.com>
2024-06-18 10:12:26 -0300

    Adicionando historico e finalizando projeto

--- assets/script.js
+++ assets/script.js
@@ -10,8 +10,28 @@ function Limpar(){
     document.getElementById('Res').innerHTML = "";  
 }
 function Calc(){
-    var Res = document.getElementById('Res').innerHTML;
+    const Res = document.getElementById('Res').innerHTML;
     if(Res){
-    document.getElementById('Res').innerHTML = eval(Res)
+    document.getElementById('Res').innerHTML = `${Res} = ${eval(Res)} <br>` 
     }else{}
+
+    let histElem = document.createElement("p");
+    histElem.classList.add("hist-calc");
+
+    histElem.innerHTML = `${Res} = ${eval(Res)}` 
+    
+    const histVis = document.getElementsByClassName('itens')[0];
+    histVis.append(histElem);
+}
+function historico(){
+    const dip = document.getElementById('disp');
+    const hist = document.getElementById('historico')
+    hist.style.display = 'block'
+    dip.style.display = 'none';
+}
+function volta(){
+    const dip = document.getElementById('disp');
+    const hist = document.getElementById('historico')
+    hist.style.display = 'none'
+    dip.style.display = 'block';
 }
